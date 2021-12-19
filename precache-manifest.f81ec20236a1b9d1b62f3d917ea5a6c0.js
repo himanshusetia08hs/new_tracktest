9d98596736da8f30d3802bbfaa54754bc8b33800
Himanshu Setia <himanshusetia17@gmail.com>
2021-12-19 19:31:48 +0530

    Updates

---
+++ precache-manifest.f81ec20236a1b9d1b62f3d917ea5a6c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2a36dd9878727567eb473f6c48a9e1",
+    "url": "/new_tracktest/index.html"
+  },
+  {
+    "revision": "267814d64e6d4cf0464b",
+    "url": "/new_tracktest/static/css/2.168a2b2b.chunk.css"
+  },
+  {
+    "revision": "04ddb8e2ec60b637ba25",
+    "url": "/new_tracktest/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "267814d64e6d4cf0464b",
+    "url": "/new_tracktest/static/js/2.cd398787.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/new_tracktest/static/js/2.cd398787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ddb8e2ec60b637ba25",
+    "url": "/new_tracktest/static/js/main.9bb81961.chunk.js"
+  },
+  {
+    "revision": "f2e8f2e542fa6cfb2324",
+    "url": "/new_tracktest/static/js/runtime-main.5239fae0.js"
+  }
+]);
