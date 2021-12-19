745b66a864d6654e37398547660fef6a73c8101a
Himanshu Setia <himanshusetia17@gmail.com>
2021-12-19 19:11:05 +0530

    main commit

---
+++ build/precache-manifest.192cbaa34a54ccbff7de758fa9f88c94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e34ea93e0762ce7bc50b89094c76d8",
+    "url": "/tracktest/index.html"
+  },
+  {
+    "revision": "267814d64e6d4cf0464b",
+    "url": "/tracktest/static/css/2.168a2b2b.chunk.css"
+  },
+  {
+    "revision": "1ef3bbadc36000e5c5f6",
+    "url": "/tracktest/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "267814d64e6d4cf0464b",
+    "url": "/tracktest/static/js/2.cd398787.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/tracktest/static/js/2.cd398787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef3bbadc36000e5c5f6",
+    "url": "/tracktest/static/js/main.3cfed769.chunk.js"
+  },
+  {
+    "revision": "ceeb04a7bae5c90f8d71",
+    "url": "/tracktest/static/js/runtime-main.9fe98d3d.js"
+  }
+]);
